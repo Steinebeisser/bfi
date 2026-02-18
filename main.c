40dc55f20f4236ea9cbde218ace12db36e7f619f
Paul Geisthardt <paul.geisthardt@gmx.de>
2026-02-18 20:40:41 +0100

    windows

--- main.c
+++ main.c
@@ -18,6 +18,98 @@ typedef struct {
 
 Vec2 get_term_width();
 
+#ifdef __WIN32
+#include <windows.h>
+
+static HANDLE hOut = INVALID_HANDLE_VALUE;
+static DWORD original_output_mode = 0;
+
+static UINT g_oldOutputCP = 0;
+static UINT g_oldInputCP = 0;
+
+bool raw_mode_enabled = false;
+static DWORD original_input_mode = 0;
+static HANDLE hIn = INVALID_HANDLE_VALUE;
+
+bool enable_raw_mode() {
+    if (raw_mode_enabled) {
+        return true;
+    }
+
+    SetConsoleCtrlHandler(NULL, TRUE);
+
+    hIn = GetStdHandle(STD_INPUT_HANDLE);
+    if (!GetConsoleMode(hIn, &original_input_mode)) {
+        return false;
+    }
+    DWORD mode = original_input_mode;
+
+    mode &= ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT | ENABLE_PROCESSED_INPUT);
+    if (!SetConsoleMode(hIn, mode)) {
+        return false;
+    }
+
+    hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut == INVALID_HANDLE_VALUE) {
+        return false;
+    }
+
+    if (!GetConsoleMode(hOut, &original_output_mode)) {
+        return false;
+    }
+    DWORD dwMode = original_output_mode;
+
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN | ENABLE_PROCESSED_OUTPUT;
+    if (!SetConsoleMode(hOut, dwMode)) {
+        return false;
+    }
+
+    g_oldOutputCP = GetConsoleOutputCP();
+    g_oldInputCP = GetConsoleCP();
+
+    if (!SetConsoleOutputCP(65001)) return false;
+    if (!SetConsoleCP(65001)) return false;
+
+    atexit(disable_raw_mode);
+    raw_mode_enabled = true;
+}
+
+void disable_raw_mode() {
+    if (raw_mode_enabled && original_input_mode != 0 && hIn != INVALID_HANDLE_VALUE) {
+        FlushConsoleInputBuffer(hIn);
+        SetConsoleMode(hIn, original_input_mode);
+        raw_mode_enabled = false;
+    }
+
+
+    if (original_output_mode != 0 && hOut != INVALID_HANDLE_VALUE) {
+        SetConsoleMode(hOut, original_output_mode);
+    }
+
+    if (g_oldOutputCP != 0) {
+        SetConsoleOutputCP(g_oldOutputCP);
+    }
+    if (g_oldInputCP != 0) {
+        SetConsoleCP(g_oldInputCP);
+    }
+}
+
+Vec2 get_term_width() {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int columns, rows;
+
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+
+    return (Vec2) {
+        .x = columns,
+        .y = rows
+    };
+}
+
+#endif
+
 
 #if defined (__unix__)
     #include <termios.h>
